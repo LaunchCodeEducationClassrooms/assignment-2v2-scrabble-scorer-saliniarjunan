a43f78f0f2abcf5141d228dbf54d45d0e62176e8
Salini Arjunan <salini_arjunan@yahoo.com>
2021-02-27 16:14:46 +0000

    Fixing scoringFunction

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -97,17 +97,17 @@ const scoringAlgorithms = [
 {
   'name':'Simple Score',
   'description':'Each letter is worth 1 point.',
-  'scorerFunction': simpleScore
+  'scoringFunction': simpleScore
 },
 {
   'name':'Bonus Vowels',
   'description':'Vowels are 3 pts, consonants are 1 pt.',
-  'scorerFunction': vowelBonusScore
+  'scoringFunction': vowelBonusScore
 },
 {
   'name':'Scrabble',
   'description':'The traditional scoring algorithm.',
-  'scorerFunction': scrabbleScore
+  'scoringFunction': scrabbleScore
 }  
 ];
 
@@ -166,7 +166,7 @@ let newPointStructure = transform(oldPointStructure);
 function runProgram() {
    let userWord = initialPrompt();
    let selectedAlgorithm = scorerPrompt();   
-   console.log(`Score for '${userWord}': ${selectedAlgorithm.scorerFunction(userWord)}\n`);   
+   console.log(`Score for '${userWord}': ${selectedAlgorithm.scoringFunction(userWord)}\n`);   
 }
 
 // Don't write any code below this line //
